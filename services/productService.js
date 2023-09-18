2ea3e6c893c40586918660bff4f3df013bd699b6
Victor Aaron Alvarez Paredes <89317849+Vaap2099@users.noreply.github.com>
2023-09-18 16:37:57 -0600

    update y delete

--- services/productService.js
+++ services/productService.js
@@ -20,8 +20,13 @@ generate() {
 
 }
 
-  create () {
-
+  create (data) {
+    const newProduct = {
+      id: faker.string.uuid(),
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   find() {
@@ -33,12 +38,26 @@ generate() {
 
   }
 
-  update() {
-
+  update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1 ) {
+      throw new Error('Product not found');
+    }
+    const product = this.products[index];
+    this.products[index]= {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
   }
 
-  delete() {
-
+  delete(id) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1 ) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
   }
 
 }
