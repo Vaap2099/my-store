dc810886625aa219a8f0eef47745c5cc8ee75e2c
Victor Aaron Alvarez Paredes <89317849+Vaap2099@users.noreply.github.com>
2023-09-18 13:05:37 -0600

    Creacion de servicios

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('./../services/productService');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -23,17 +16,9 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id==='999') {
-    res.status(404).json({
-      message: 'NOT FOUND'
-    });
-  } else {
-  res.status(200).json ({
-    id, name: "Product N",
-    price: 500
-  })
-  }
-})
+  const product = service.findOne(id);
+  res.json (product);
+});
 
 router.post('/', (req, res) => {
   const body = req.body;
