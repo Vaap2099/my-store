08bc9e904c381fcdbcd3899ab0e45f4766a083ca
Victor Aaron Alvarez Paredes <89317849+Vaap2099@users.noreply.github.com>
2023-09-19 14:52:13 -0600

    despliegue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require ('./middlewares/ErrorHandler')
@@ -7,6 +8,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No Permitido'));
+    }
+  }
+}
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express')
